d86ae74da237aa241229bf4a5af63a55fabb9d42
MayurGH <mayurkg@gmail.com>
2020-11-28 21:35:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println('\u00DA');
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+    }
+}
